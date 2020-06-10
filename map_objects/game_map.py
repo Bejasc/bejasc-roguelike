e52f63d85f35670963a4cbe914e41b3a4ba6d954
Ben Schulz <benjamin.schulz94@gmail.com>
2020-06-10 22:20:06 +0930

    Change linter settings

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -8,8 +8,7 @@ class GameMap:
         self.tiles = self.initialize_tiles()
 
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)]
-                 for x in range(self.width)]
+        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
 
         tiles[30][22].blocked = True
         tiles[30][22].block_sight = True
